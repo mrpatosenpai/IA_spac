daffa05588e025fab48be5ec89b4aa6a2d186e27
mrpatosenpai <garciacacher@gmail.com>
2024-08-26 12:43:03 -0500

    probando ia

--- main.py
+++ main.py
@@ -25,5 +25,5 @@ def upload_file():
         
         return jsonify(results), 200
 
-if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=5000, debug=True)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
